46ba3d3e9d4c5bc2ab85dc0597ae14f77c80c160
DavidM <dmataguerra@outlook.com>
2025-09-13 23:54:02 -0600

    Initialized TypeORM for Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: +(process.env.PORT || 5432),
+      username: 'postgres',
+      password: process.env.PASS,
+      database: process.env.NAME,
+      entities: [],
+      autoLoadEntities: true, 
+      synchronize: true,
+    }),
+    EmployeesModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
