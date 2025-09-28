2c55bd65913abae707678644106d5ba962f10791
DavidM <dmataguerra@outlook.com>
2025-09-27 19:49:51 -0600

    feat(auth): implement role-based access control with guards and decorator.

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,29 @@
+
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from '../decorators/roles.decorators';
+import { User } from '../entities/user.entity';
+import { access } from 'fs';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    //Reflector == Metadatos
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user: User = request.user;
+    return this.matchRoles(roles, user.userRoles);
+  }
+  matchRoles(roles: string[], userRoles : string[]) {
+    let access = false;
+    userRoles.forEach((userRole) => {
+        if (roles.includes(userRole)) access = true;
+  })
+    return access;
+}
+}
--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -6,6 +6,8 @@ import { NotFoundException } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { UserData } from 'src/auth/decorators/user.decorators';
 import { User } from 'src/auth/entities/user.entity';
+import { Roles } from 'src/auth/decorators/roles.decorators';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @UseGuards(AuthGuard)
 @Controller('providers')
@@ -17,7 +19,8 @@ export class ProvidersController {
     return this.providersService.create(createProviderDto);
   }
 
-
+  @Roles(["Admin"])
+  @UseGuards(RolesGuard)
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes("Employee")) throw new UnauthorizedException('No estas autorizado, solo Admins y Managers.');
